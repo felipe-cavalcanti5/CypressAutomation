7e7b9f0354379ea5d9d4466a13d20b1a8542dadb
Felipe Cavalcanti <felipe.araujo@elevaty.com.br>
2025-08-23 11:14:53 -0300

    conclusão módulo 6

---
+++ cypress/integration/exemples/exercice01.js
@@ -0,0 +1,22 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+
+    var count = []
+    for(var i = 0; i<n; i ++){
+        if(i ===0){
+            count += 0
+        }
+    }
+    
+    
+    
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
+
